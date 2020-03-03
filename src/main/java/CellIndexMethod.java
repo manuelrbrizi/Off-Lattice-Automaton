3c449bfd131b73c76e8fccd08c8dbdd154eef85c
Manuel <manuel.rbrizi@gmail.com>
2020-03-03 13:52:12 -0300

    Interface update

--- src/main/java/CellIndexMethod.java
+++ src/main/java/CellIndexMethod.java
@@ -1,5 +1,26 @@
+import implementations.GridImpl;
+import interfaces.Grid;
+import interfaces.Particle;
+
+import java.util.List;
+
 public class CellIndexMethod {
     public static void main(String[] args){
-        System.out.println("HOLA SS!");
+
+    }
+
+    public void doMethod(int L, int M, int N, int Rc){
+        Grid grid = new GridImpl(L, M);
+        int xCellPosition = 0, yCellPosition = 0, cellQuantity = M * M;
+
+        for(int i = 0; i < cellQuantity; i++){
+            if(i == M-1){
+                xCellPosition = 0;
+                yCellPosition++;
+            }
+            else{
+
+            }
+        }
     }
 }
--- src/main/java/interfaces/Particle.java
+++ src/main/java/interfaces/Particle.java
@@ -1,9 +1,14 @@
 package interfaces;
 
+import java.util.List;
+
 public interface Particle {
 
     double calculateDistance(Particle p);
     double getX();
     double getY();
     double getRadius();
+    int getId();
+    List<Particle> getNeighbours();
+    void setNeighbours(List<Particle> neighbours);
 }
